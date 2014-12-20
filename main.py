ab4db23ff37236daf75e7f5fa2337b18f3e83513
Suhas <suhas.hm109@gmail.com>
2014-12-20 02:32:43 -0600

    Getting common trending items from World and US

--- main.py
+++ main.py
@@ -18,18 +18,25 @@ def get_localized_trends():
     US_WOE = 23424977
 
     world_trends = twitter_api.trends.place(_id=WORLD_WOE)
-    print "world trends are"
-    print json.dumps(world_trends, indent=1)
+    #print "world trends are"
+    #print json.dumps(world_trends, indent=1)
 
     us_trends = twitter_api.trends.place(_id=US_WOE)
-    print "US trends are"
-    print json.dumps(us_trends, indent=1)
+    #print "US trends are"
+    #print json.dumps(us_trends, indent=1)
+
+    world_trends_set = set([trend['name'] for trend in world_trends[0]['trends']])
+    us_trends_set = set([trend['name'] for trend in us_trends[0]['trends']])
+
+    common_trends_set = world_trends_set.intersection(us_trends_set)
+    print "common trends are"
+    print common_trends_set
 
 def main():
     print "Authenticating with Twitter"
 
     print
-
+    # get trends based on yahoo where on earth ID and compute common trending items
     get_localized_trends()
 
 if __name__ == '__main__':
