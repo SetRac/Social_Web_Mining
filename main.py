97ccd383d212cf63190ba74474db72f444ceb257
Suhas <suhas.hm109@gmail.com>
2014-12-23 05:02:40 -0600

    implemented get first tending tweet details

--- main.py
+++ main.py
@@ -29,15 +29,48 @@ def get_localized_trends():
     us_trends_set = set([trend['name'] for trend in us_trends[0]['trends']])
 
     common_trends_set = world_trends_set.intersection(us_trends_set)
-    print "common trends are"
-    print common_trends_set
+    return common_trends_set
+
+def get_trend_details(first_trend):
+    q = first_trend
+    count = 100
+    search_results = twitter_api.search.tweets(q=q, count=count)
+    statuses = search_results['statuses']
+
+    for _ in range(5):
+        print "length of statuses ", len(statuses)
+        try:
+            next_results = search_results['search_metadata']['next_results']
+        except KeyError, e:
+            break
+
+        # construct a dict if next_results exist, this is cursoring aka pagination for twitter streams
+
+        twitter_dict = dict([kv.split("=") for kv in next_results[1:].split("&")])
+        search_results = twitter_api.search.tweets(**twitter_dict)
+        statuses += search_results['statuses']
+
+    print json.dumps(statuses[0], indent=1)
 
 def main():
     print "Authenticating with Twitter"
-
     print
+
     # get trends based on yahoo where on earth ID and compute common trending items
-    get_localized_trends()
+    common_trends_set = get_localized_trends()
+
+    print "number of common trends are ", len(common_trends_set)
+    print "common trends are"
+    for trends in common_trends_set:
+        print trends
+
+    # retrieve first common trend
+    for first_trend in common_trends_set:
+        break
+    print "first common trend is ", first_trend
+
+    # get tweet details for first trend
+    get_trend_details(first_trend)
 
 if __name__ == '__main__':
     main()
