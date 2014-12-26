46cbd75cb810dc8cc4488bb300e2c2c170672aae
Suhas <suhas.hm109@gmail.com>
2014-12-26 01:26:58 -0600

    comparision of likes between 2 fan pages

--- facebook_mining.py
+++ facebook_mining.py
@@ -8,6 +8,9 @@ ACCESS_TOKEN = ''
 def print_json(ob):
     print json.dumps(ob, indent=1)
 
+def int_format(n):
+    return "{:,}".format(n)
+
 def get_likes_http():
     base_url = 'https://graph.facebook.com/me'
 
@@ -34,6 +37,15 @@ def get_my_fb_details(FB):
     # get facebook search details for Web Mining
     print_json(FB.request("search", {'q': 'Web Mining', 'type': 'page'}))
 
+def compare_fanpage_likes(FB):
+
+    # comparing likes between pepsi and coke fan pages
+    pepsi_id = '56381779049'  # 'PepsiUS' can be used instead of numeric id
+    coke_id = '40796308305'   # 'CocaCola' can be used instead of numeric id
+
+    print "Pepsi likes", int_format(FB.get_object(pepsi_id)['likes'])
+    print "Coke likes", int_format(FB.get_object(coke_id)['likes'])
+
 def main():
     print "Facebook Mining"
 
@@ -44,7 +56,10 @@ def main():
     FB = connect_facebook_graph_api()
 
     # basic graph_api queries, get one's details
-    get_my_fb_details(FB)
+    #get_my_fb_details(FB)
+
+    # comparing likes between fan pages
+    compare_fanpage_likes(FB)
 
 if __name__ == '__main__':
     main()
